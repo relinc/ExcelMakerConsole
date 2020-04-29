dd6396c1daa3cdd76a552c8669d3ef66eb277fb9
Mark Halonen <halonen.mark@gmail.com>
2020-04-29 17:29:51 -0400

    Add unit tests to iterate on Excel outputs faster

---
+++ TestExcelMaker/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestExcelMaker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestExcelMaker")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e03855bd-2f38-45f7-b89c-c1d361dd8ba7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestExcelMaker/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExcelMakerConsole;
+using System.IO;
+
+namespace TestExcelMaker
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        
+
+        [TestMethod]
+        public void TestReferenceSample()
+        {
+            string solution_dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            
+            ExcelFileMaker maker = new ExcelFileMaker(Path.Combine(solution_dir, "JobFiles", "OneReferenceSampleJobFile"));
+
+            maker.exportPath = Path.Combine(solution_dir, "OneReferenceSampleJobFile.xlsx");
+
+            maker.exportExcelFile();
+        }
+
+        [TestMethod]
+        public void TestOneSample()
+        {
+            string solution_dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            ExcelFileMaker maker = new ExcelFileMaker(Path.Combine(solution_dir, "JobFiles", "OneSampleJobFile"));
+
+            maker.exportPath = Path.Combine(solution_dir, "OneSampleJobFile.xlsx");
+
+            maker.exportExcelFile();
+        }
+
+        [TestMethod]
+        public void TestMultipleEverything()
+        {
+            string solution_dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+
+            ExcelFileMaker maker = new ExcelFileMaker(Path.Combine(solution_dir, "JobFiles", "MultipleEverythingJobFile"));
+
+            maker.exportPath = Path.Combine(solution_dir, "MultipleEverythingJobFile.xlsx");
+
+            maker.exportExcelFile();
+        }
+    }
+}
